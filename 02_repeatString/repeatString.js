7b21b9dd810f566341bfa5d67c83b17e462d9073
Bobby Ta <bobbyta74@gmail.com>
2022-10-06 15:08:47 +0200

    Do 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(strinput, times) {
+    let strout = "";
+    if (times < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < times; i++) {
+        strout += strinput;
+    }
+    return strout;
 };
 
 // Do not edit below this line
