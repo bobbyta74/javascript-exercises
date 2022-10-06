bc4ed5368788025cb219b8d3233c32d183787018
Bobby Ta <bobbyta74@gmail.com>
2022-10-06 15:24:43 +0200

    Do 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(strinput) {
+    let reversed = "";
+    let myarray = Array.from(strinput);
+    for (let i = 1; i <= myarray.length; i++) {
+        reversed += myarray[myarray.length - i];
+    }
+    return reversed;
 };
 
 // Do not edit below this line
