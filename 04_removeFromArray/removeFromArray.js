3a6825fb101215605f3784b3ef048feae624a445
Bobby Ta <bobbyta74@gmail.com>
2022-10-14 11:30:05 +0200

    Learned 04_removeFromArray from solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    //1st parameter is the whole list
+    const array = args[0];
+    let newArray = [];
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
