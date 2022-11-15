227c42b017b5c5aff590df9a58943b220981caab
Bobby Ta <bobbyta74@gmail.com>
2022-11-15 10:21:47 +0100

    Do 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let opposite = "";
+    let cleanstring = "";
+    //Removes punctuation
+    cleanstring = text.replace(/[^\w\']|_/g, "");
+    cleanstring = cleanstring.toLowerCase();
+    for (let i = (cleanstring.length - 1); i >= 0; i--) {
+        opposite += cleanstring[i];
+    }
+    return (opposite == cleanstring);
 };
 
 // Do not edit below this line
