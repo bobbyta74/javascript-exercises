b7c7b337229579f1ef973980271f19ce1fa6b859
Bobby Ta <bobbyta74@gmail.com>
2022-11-15 09:53:20 +0100

    Do 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	for (let i of array) {
+    sum += i;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+  for (let i of array) {
+    product *= i;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let product = 1;
+	for (let i = num; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
